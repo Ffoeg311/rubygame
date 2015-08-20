d62bc3fa1c0003fff0b2c417e6e9bb6d7f735015
Geoff Porter <geoff.porter@gmail.com>
2015-08-19 23:33:33 -0400

    Worked on animation.

--- gamewindow.rb
+++ gamewindow.rb
@@ -4,10 +4,9 @@ class Entity
 	attr_accessor :x, :y, :z, :animations	
 
 	def initialize(image_url, x, y, z)
+		@image_url = image_url
 		@image = Gosu::Image.new(image_url)
-		@x = x
-		@y = y
-		@z = z
+		@x, @y, @z = x, y, z
 		@animations = {} 
 	end	
 
@@ -19,12 +18,12 @@ class Entity
 		if @current_animation.nil?
 			@image.draw(x, y, z)
 		else
-			
+			@current_animation.draw(x, y, z)	
 		end
 	end
 
-	def add_animation(animation_name, animation_speed, tile_width, tile_height, offset, tile_start, tile_end)
-		animations[animation_name] = Animation.new
+	def add_animation(animation_name, speed, tile_width, tile_height, first, last)
+		animations[animation_name] = Animation.new(@image_url, speed, tile_width, tile_height, first, last)
 	end
 
 	def set_current_animation(next_animation)
@@ -34,8 +33,14 @@ class Entity
 end
 
 class Animation
-	def initialize
-		#TODO
+	def initialize(image_url, speed, tile_width, tile_height, first, last)
+		@speed = speed
+		@tiles = Gosu::Image::load_tiles(image_url, tile_width, tile_height).slice(first..last)
+	end
+
+	def draw(x, y, z)
+		img = @tiles[Gosu::milliseconds / @speed % @tiles.size]
+		img.draw(x, y, z)
 	end
 end
 
@@ -43,7 +48,10 @@ class GameWindow < Gosu::Window
 	def initialize
 		super 250, 200, fullscreen: true
 		@background_entity = Entity.new("media/arbre.png", 0, 0, 0)
+		# Create the wizard and make them walk
 		@wizard_entity = Entity.new("media/wizard.png", 0, 0, 0)
+		@wizard_entity.add_animation(:walk, 100, 32, 32, 20, 30)
+		@wizard_entity.set_current_animation(:walk)
 	end
 	
 	def update
